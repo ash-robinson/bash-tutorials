33a991699c88f0f574a43cfb2b3d46c7c71012a0
Ash Robinson <arobinson@honeycomb.co.uk>
2018-05-01 13:59:33 +0100

    simple examples and added cheatsheet

--- task-1/answers.sh
+++ task-1/answers.sh
@@ -5,4 +5,12 @@
 
 echo "answer 1:  $(./task.sh ...........)"
 echo "answer 2:  $(./task.sh ...........)"
+echo "answer 3:  $(./task.sh ...........)"
+echo "answer 4:  $(./task.sh ...........)"
+echo "answer 5:  $(./task.sh ...........)"
+echo "answer 6:  $(./task.sh ...........)"
+echo "answer 7:  $(./task.sh ...........)"
+echo "answer 8:  $(./task.sh ...........)"
+echo "answer 9:  $(./task.sh ...........)"
+echo "answer 10:  $(./task.sh ...........)"
 
