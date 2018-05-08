7469b668c2fcb02e548782ecd5eeeab7ee216826
Ash Robinson <arobinson@honeycomb.co.uk>
2018-05-08 17:29:23 +0100

    task-2: grep and basic regex

---
+++ task-2/answers.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+## build your answer command like below:
+## echo "example: $(<enter your command here>)"
+
+echo "answer 1:  $(...........)"
+echo "answer 2:  $(...........)"
+echo "answer 3:  $(...........)"
+echo "answer 4:  $(...........)"
+echo "answer 5:  $(...........)"
+echo "answer 6:  $(...........)"
+echo "answer 7:  $(...........)"
+echo "answer 8:  $(...........)"
+
+echo "extension 1:  $(...........)"
+echo "extension 2:  $(...........)"
+echo "extension 3:  $(...........)"
+echo "extension 4:  $(...........)"
+echo "extension 5:  $(...........)"
+
